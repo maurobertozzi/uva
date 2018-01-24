cad51a653d63a0b77f50fdb05d3138421921a0f2
maurobertozzi <maurobertozzi11@gmail.com>
2018-01-24 11:09:04 +0100

    update

---
+++ orderRide1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="orderride1style.css">
+	<script src="script1.js"></script>
+<title>Document</title>
+</head>
+<body>
+<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<p id='latitudeAndLongitude'></p>
+<p id='address'></p> 
+
+
+<h2>Start?</h2>
+<input type="text">
+<h2>Destination?</h2>
+<input type="text">
+
+<h2>When?</h2>
+
+
+
+<h5>Now</h5>
+<button id="ContinueButton">Continue</button>
+</body>
+</html>
---
+++ orderRide2.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>order ride</title>
+	<link rel="stylesheet" href="orderride1style.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<h5>&#8592;back</h5>
+<h2>How many people?</h2>
+
+<div class="slidecontainer">
+  <input type="range" min="1" max="5" value="50" class="slider" id="myRange">
+  <p><span id="demo"></span></p>
+</div>
+<h2>Which route?</h2>
+<div class="btn-group">
+  <button id="buttonCheapest">Cheapest</button>
+  <button id="buttonFastest">Fastest</button>
+  <button id="buttonScenic">Scenic</button>
+</div>
+<img src="1.jpg" alt="">
+<button id="ContinueButton">Continue</button>
+<script src="script.js"></script>
+</body>
+</html>
