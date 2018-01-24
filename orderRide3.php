10107bd5af092650a8e3c6d620a8ea49c8d45d88
NankoPrinzhorn <memaybeme@hotmail.com>
2018-01-24 11:40:39 +0100

    3e pagina done

---
+++ orderRide3.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="orderride3style.css">
+<title>Document</title>
+</head>
+<body>
+	<p id="back"> &lt; Back</p>
+	<p id="cancel">Cancel</p>
+	<h1 id="title">Your car is on its way!</h1>
+	<img id="map" src="https://www.wired.com/wp-content/uploads/2016/11/GoogleMapTA.jpg">
+	<p id="drivenBy">You will be driven by car</p>
+	<p id="drivenByAnswer">0161</p>
+	<p id="drivenBy">Your car will be here in</p>
+	<p id="drivenByAnswer">0:35 hour</p>
+</body>
+</html>
